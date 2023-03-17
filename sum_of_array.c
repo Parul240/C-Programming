2a6d943a4e5251d0432045757e4d582dbe4bd2bc
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-17 18:23:35 +0530

    Level : Easy

---
+++ sum_of_array.c
@@ -0,0 +1,25 @@
+PROBLEM:
+
+/*
+Write a program in 'C' to find the sum of array which contain 10 elemenets
+*/
+
+
+SOLUTION:
+
+#include <stdio.h>
+int main()
+{
+     int a[10],i,s=0;
+     printf("Enter 10 Numbers:\n");
+     for(i=0;i<10;i++)
+     {
+          scanf("%d",&a[i]);
+          s=s+a[i];
+     }
+     printf("\nNumbers Stored in Array : \n");
+     for(i=0;i<10;i++)
+        printf("a[%d]=%d\n",i,a[i]);
+     printf("\nSum of Integer Numbers : %d",s);
+     return 0;
+}
